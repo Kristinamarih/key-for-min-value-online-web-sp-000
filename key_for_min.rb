8f33d8f09343787480b1586c55e3a1cdcc2c8fac
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-03-28 23:02:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_value = 0 
   lowest_key = nil
-name_hash.collect do |name, value|
+name_hash.collect do |key, value|
   if lowest_value == 0 || value < lowest_value
     value = lowest_value
     lowest_key = key 
